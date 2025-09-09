25c3e3504465e210fceac215eec50ddb0606401d
TaSato-MSFT <tasato@microsoft.com>
2025-09-09 22:06:32 +0900

    Commit

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
-if( !function_exists('apache_request_headers') ) {
-
-function apache_request_headers() {
-  $arh = array();
-  $rx_http = '/\AHTTP_/';
-  foreach($_SERVER as $key => $val) {
-    if( preg_match($rx_http, $key) ) {
-      $arh_key = preg_replace($rx_http, '', $key);
-      $rx_matches = array();
-      // do some nasty string manipulations to restore the original letter case
-      // this should work in most cases
-      $rx_matches = explode('_', $arh_key);
-      if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
-        foreach($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
-        $arh_key = implode('-', $rx_matches);
-      }
-      $arh[$arh_key] = $val;
-    }
-  }
-  return( $arh );
-  }
+if (!function_exists('getallheaders')) 
+{ 
+    function getallheaders() 
+    { 
+       $headers = array (); 
+       foreach ($_SERVER as $name => $value) 
+       { 
+           if (substr($name, 0, 5) == 'HTTP_') 
+           { 
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+           } 
+       } 
+       return $headers; 
+    } 
 }
+
+$heads = getallheaders(); 
+
+foreach ($heads as $head => $value) { 
+    echo "$head: $value <br />\n"; 
+} 
+
+?> 
