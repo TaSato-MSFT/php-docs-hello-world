cca641e89c682d749a8f996de6774ba785ec47f8
TaSato-MSFT <tasato@microsoft.com>
2025-09-17 10:39:42 +0900

    Commit

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-    echo 'ユーザーエージェント: ' . $_SERVER['HTTP_USER_AGENT'] . '<br>';
-    echo 'リクエストメソッド: ' . $_SERVER['REQUEST_METHOD'] . '<br>';
-    echo 'クライアントIPアドレス: ' . $_SERVER['REMOTE_ADDR'] . '<br>';
-    echo 'ClientCertificate: ' . $_SERVER['HTTP_X_ARR_ClientCert'] . '<br>';
-    echo 'DummyCertificate: ' . $_SERVER['HTTP_X_Hoge_ClientCert'] . '<br>';
+    var_dump(getallheaders());
 ?>
